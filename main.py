710befcf73ddce84da8307658b0abd6d492f19af
vorkutavorkutlag <royzurakipiani@gmail.com>
2026-01-16 01:38:47 +0100

    main: refined basic commands logic. created empty sreality script

--- main.py
+++ main.py
@@ -2,21 +2,82 @@ import os
 
 import discord
 import dotenv
+import json
+from pprint import pprint
+from enum import Enum
+from discord.ext import commands, tasks
+
+Context = discord.ext.commands.context.Context
 
 dotenv.load_dotenv()
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-
-
-class Client(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}')
+DISCORD_TOKEN = os.getenv('BOT_TOKEN')
 
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
+intents.guilds = True
 
-client = Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+glob_config = {}
 
 
-if __name__ == "__main__":
-    client.run(BOT_TOKEN)
+class ConfigKeys(Enum):
+    DEFAULT_CHANNEL = "default_channel"
+
+
+def load_config() -> dict:
+    try:
+        with open("config.json", 'r') as f:
+            try:
+                return json.load(f)
+            except json.decoder.JSONDecodeError:
+                return {}
+    except FileNotFoundError:
+        with open("config.json", 'w'):
+            pass
+        return {}
+
+
+def save_config(config: dict) -> None:
+    with open("config.json", 'w') as f:
+        json.dump(config, f)
+
+
+@bot.event
+async def on_ready():
+    print(f"We are {bot.user.name}")
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def setchannel(ctx: Context, *args):
+    global glob_config
+    glob_config = load_config()
+
+    channel_id = int(args[0][2:-1])
+    channel = bot.get_channel(channel_id)
+
+    if type(channel) != discord.channel.TextChannel:
+        await ctx.send("Invalid channel.")
+
+    glob_config[ConfigKeys.DEFAULT_CHANNEL.value] = channel_id
+    save_config(glob_config)
+
+    await ctx.send("Successfully registered channel.")
+
+
+@tasks.loop(hours=1)
+async def scrape_sreality():
+    pass
+
+
+bot.run(token=DISCORD_TOKEN)
